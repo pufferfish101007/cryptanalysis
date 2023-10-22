fb29c8c090b269a172e78b3efebb3e532d2197e2
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2023-10-22 13:37:46 +0000

    add info to fitness values

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
     import { normalizedIoC, monogramFitness, monogramFrequencies, tetragramFitness, entropy } from './lib/fitness.js';
     import Table from './components/Table.vue';
+    import Info from './components/Info.vue';
     import { ref, watch } from 'vue';
     const cyphertext = ref('');
     const plaintext = ref('');
@@ -15,10 +16,12 @@
     <div class="container vertical">ciphertext:<textarea v-model="cyphertext"></textarea></div>
     <div class="container vertical">plaintext:<textarea v-model="plaintext" disabled></textarea></div>
     <div>
-      <div>Monogram fitness: {{ monogramFitness(cyphertext).toFixed(2) }}</div>
-      <div>Tetragram fitness: {{ tetragramFitness(cyphertext).toFixed(2) }}</div>
-      <div>Index of coincidence: {{ normalizedIoC(cyphertext).toFixed(2) }}</div>
-      <div>Entropy: {{ entropy(cyphertext).toFixed(2) }}</div>
+      <div>Monogram fitness: {{ monogramFitness(cyphertext).toFixed(2) }} <Info>A monogram fitness close to 1 is simlar to standard English; close to 0 is not similar.</Info></div>
+      <div>Tetragram fitness: {{ tetragramFitness(cyphertext).toFixed(2) }} <Info>Less negative is a better fit to
+English; more negative is a worse fit.
+</Info></div>
+      <div>Index of coincidence: {{ normalizedIoC(cyphertext).toFixed(2) }} <Info>Typical English IoC: 1.75</Info></div>
+      <div>Entropy: {{ entropy(cyphertext).toFixed(2) }} <Info>A higher entropy value means it is more predictable and less random.</Info></div>
     </div>
     <Table :headings="['letter', 'count', 'relative frequency']" :data="Object.entries(monogramFrequencies(cyphertext)).map(a => [...a, ((a[1] / cyphertext.length) || 0).toFixed(3)])"></Table>
   </div>
---
+++ src/components/Info.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import { ref } from 'vue';
+    const dialog = ref();
+</script>
+
+<template>
+    <span @click="dialog.showModal()">🛈</span>
+    <dialog ref="dialog">
+        <slot default>info should be here but none was provided</slot>
+        <br>
+        <button autofocus @click="dialog.close()">close</button>
+    </dialog>
+</template>
+
+<style scoped>
+    span {
+        cursor: pointer;
+    }
+    button {
+        margin-top: 1em;
+    }
+    ::backdrop {
+        background-color: black;
+        opacity: 0.5;
+        transition: 1s;
+    }
+</style>
